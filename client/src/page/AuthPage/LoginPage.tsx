7e1499b4dddf89d862aaa9e7f5ec027686452921
QuanPC131002 <quanthuoclao1310@gmail.com>
2025-06-16 16:00:29 +0700

    feat: add login and register form UI

--- client/src/page/AuthPage/LoginPage.tsx
+++ client/src/page/AuthPage/LoginPage.tsx
@@ -1,10 +1,189 @@
 import { Button } from "@/components/ui/button";
-import React from "react";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
+import { Tabs, TabsContent, TabsList, TabsTrigger} from "@/components/ui/tabs";
+import { faGoogle, faInstagram, faLinkedin, faTwitter } from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { Lock, Mail, Phone, User } from "lucide-react";
+import { Link } from "react-router-dom";
 
 export default function LoginPage() {
   return (
     <div className="flex w-screen h-screen items-center justify-center ">
-      <Button className="bg-green-300">LoginPage</Button>
+      <Tabs defaultValue="login" className="w-[550px] min-h-[400px]">
+        {/* Navigate */}
+        <TabsList className="grid grid-cols-2 w-full">
+          <TabsTrigger
+           value="login"
+           className="data-[state=active]:bg-blue-800 data-[state=active]:text-white p-2 border border-b-[#ccc] rounded-none cursor-pointer">
+            ĐĂNG NHẬP
+          </TabsTrigger>
+          <TabsTrigger
+           value="register"
+           className="data-[state=active]:bg-blue-800 data-[state=active]:text-white p-2 border border-b-[#ccc] rounded-none cursor-pointer">
+            ĐĂNG KÝ
+          </TabsTrigger>
+        </TabsList>
+        {/* End Navigate */}
+
+        {/* Login */}
+        <TabsContent value="login" className="space-y-6 p-6 rounded-lg shadow-md">
+          <div className="space-y-2">
+            <Label htmlFor="email">
+              <span className="text-red-600">*</span> Email
+            </Label>
+              <div className="relative">
+                <Mail className="absolute left-3 top-1/2 transform -translate-y-1/2 w-4 h-4" />
+                <Input
+                  type="email"
+                  placeholder="Email"
+                  className="pl-10"
+                />
+              </div>
+          </div>
+
+          <div className="space-y-2">
+            <Label htmlFor="password">
+              <span className="text-red-600">*</span> Password
+            </Label>
+            <div className="relative">
+                <Lock className="absolute left-3 top-1/2 transform -translate-y-1/2 w-4 h-4" />
+                <Input
+                  type="password"
+                  placeholder="Mật khẩu"
+                  className="pl-10"
+                />
+            </div>
+          </div>
+
+          <div className="text-blue-600 text-sm hover:underline block text-left">
+            <Link to='#'>Quên mật khẩu?</Link>
+          </div>
+          <div className="flex flex-col items-center space-y-3 mt-6">
+            <Button className="bg-blue-700 text-white hover:bg-blue-800 hover:shadow-lg hover:text-white cursor-pointer">Đăng Nhập</Button>
+          </div>
+          <div className="items-center justify-center flex space-x-5 text-4xl">
+            <FontAwesomeIcon
+              icon={faGoogle}
+              className="text-red-600 hover:scale-110 transition duration-300 cursor-pointer"
+            />
+            <FontAwesomeIcon
+              icon={faLinkedin}
+              className="text-blue-600 hover:scale-110 transition duration-300 cursor-pointer"
+            />
+            <FontAwesomeIcon
+              icon={faTwitter}
+              className="text-sky-400 hover:scale-110 transition duration-300 cursor-pointer"
+            />
+        </div>
+        </TabsContent>
+        {/* End Login */}
+
+        {/* Register */} 
+        <TabsContent value="register" className="p-6 rounded-lg shadow-md">
+        <div className="grid grid-cols-2 gap-4">
+          <div className="space-y-6">
+
+            <div>
+              <Label htmlFor="name" className="my-2">
+                <span className="text-red-600">*</span> Họ tên
+              </Label>
+              <div className="relative">
+                <User className="absolute left-3 top-1/2 transform -translate-y-1/2 w-4 h-4" />
+                <Input
+                  type="text"
+                  placeholder="Họ tên"
+                  className="pl-10"
+                />
+              </div>
+            </div>
+
+            <div>
+              <Label htmlFor="password" className="my-2">
+                <span className="text-red-600">*</span> Mật khẩu
+              </Label>
+              <div className="relative">
+                <Lock className="absolute left-3 top-1/2 transform -translate-y-1/2 w-4 h-4" />
+                <Input
+                  type="password"
+                  placeholder="Mật khẩu"
+                  className="pl-10"
+                />
+              </div>
+            </div>
+            
+            <div>
+              <Label htmlFor="phone" className="my-2">
+                <span className="text-red-600">*</span> Số điện thoại
+              </Label>
+              <div className="relative">
+                <Phone className="absolute left-3 top-1/2 transform -translate-y-1/2 w-4 h-4" />
+                <Input
+                  type="phone"
+                  placeholder="Số điện thoại"
+                  className="pl-10"
+                />
+              </div>
+            </div>
+          </div>
+
+          <div className="space-y-6">
+              <div>
+                <Label htmlFor="email" className="my-2">
+                  <span className="text-red-600">*</span> Email
+                </Label>
+                <div className="relative">
+                  <Mail className="absolute left-3 top-1/2 transform -translate-y-1/2 w-4 h-4" />
+                  <Input
+                    type="email"
+                    placeholder="Email"
+                    className="pl-10"
+                  />
+                </div>
+              </div>
+
+              <div>
+                <Label htmlFor="confirm-password" className="my-2">
+                  <span className="text-red-600">*</span> Xác nhận mật khẩu
+                </Label>
+                <div className="relative">
+                  <Lock className="absolute left-3 top-1/2 transform -translate-y-1/2 w-4 h-4" />
+                  <Input
+                    type="password"
+                    placeholder="Xác nhận mật khẩu"
+                    className="pl-10"
+                  />
+                </div>
+              </div>
+
+              <div className="space-y-2 mb-8">
+                <Label htmlFor="sex" className="my-2">
+                  <span className="text-red-600">*</span> Giới tính
+                </Label>
+                <div className="relative">
+                  <User className="absolute left-3 top-1/2 -translate-y-1/2 w-4 h-4" />
+                  <Select>
+                  <SelectTrigger className="w-full pl-10">
+                    <SelectValue placeholder="Giới tính" />
+                  </SelectTrigger>
+                  <SelectContent className="z-50 max-h-[100px] overflow-y-auto bg-white shadow-md">
+                    <SelectItem value="boy" className="hover:bg-blue-600 hover:text-white">Nam</SelectItem>
+                    <SelectItem value="girl" className="hover:bg-blue-600 hover:text-white">Nữ</SelectItem>
+                    <SelectItem value="other" className="hover:bg-blue-600 hover:text-white">Khác</SelectItem>
+                  </SelectContent> 
+                </Select>
+                </div>
+              </div>
+
+          </div>
+        </div>
+          <div className="flex flex-col items-center space-y-3 mt-6">
+            <Button className="bg-blue-700 text-white hover:bg-blue-800 hover:shadow-lg hover:text-white cursor-pointer">Đăng Ký</Button>
+          </div>
+        </TabsContent>
+        {/* End Register */}
+      </Tabs>
     </div>
   );
-}
+} 
