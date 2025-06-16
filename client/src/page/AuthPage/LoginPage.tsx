6a506d32485c985153007f487cb4f240e32425bc
Nguyễn Văn Ngọc Bảo <nngocbbao280604@gmail.com>
2025-06-16 09:39:22 +0700

    initialize prj

---
+++ client/src/page/AuthPage/LoginPage.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+import React from "react";
+
+export default function LoginPage() {
+  return (
+    <div className="flex w-screen h-screen items-center justify-center ">
+      <Button className="bg-green-300">LoginPage</Button>
+    </div>
+  );
+}
