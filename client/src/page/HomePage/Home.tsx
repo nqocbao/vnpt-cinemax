01b0ea56a799e9ade5f83041dd6fb33d565d9d0d
QuanPC131002 <quanthuoclao1310@gmail.com>
2025-06-19 14:21:05 +0700

    feat: add homepage slider component

---
+++ client/src/page/HomePage/Home.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Slider from './Slider'
+import Navbar from '../components/Navbar'
+
+const Home = () => {
+  return (
+    <div>
+        <Navbar />
+      <Slider />
+    </div>
+  )
+}
+
+export default Home
