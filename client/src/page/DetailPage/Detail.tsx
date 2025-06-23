ae6bc7d3ed7a8149936f9c357e56be413b8b8de3
QuanPC131002 <quanthuoclao1310@gmail.com>
2025-06-23 16:49:41 +0700

    feat: add component Trailer layout to detailpage

---
+++ client/src/page/DetailPage/Detail.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Trailer from './Trailer'
+import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
+
+const Detail = () => {
+  return (
+    <div>
+        <Navbar />
+      <Trailer />
+      <Footer />
+    </div>
+  )
+}
+
+export default Detail
